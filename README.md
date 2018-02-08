f17246d88315233912e5c6128bccf2811e419150
Garrett Berg <googberg@gmail.com>
2018-02-08 08:49:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# expect_macro: macro to replace expect/unwrap
+
+[![Build Status](https://travis-ci.org/vitiral/expect_macro.svg?branch=master)](https://travis-ci.org/vitiral/expect_macro)
+[![Build status](https://ci.appveyor.com/api/projects/status/vgis54solhygre0n?svg=true)](https://ci.appveyor.com/project/vitiral/expect_macro)
+[![Docs](https://docs.rs/expect_macro/badge.svg)](https://docs.rs/expect_macro)
+
+This macro allows you to write `expect!(my_result)` or `expect!(my_result, "error: {}", my_result)`
+
+**See the [library docs](https://docs.rs/expect_macro) for more information**
+
+
+# LICENSE
+The source code in this repository is Licensed under either of
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or
+  http://opensource.org/licenses/MIT)
+
+at your option.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+/* Copyright (c) 2018 Garrett Berg, vitiral@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+ * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+ * http://opensource.org/licenses/MIT>, at your option. This file may not be
+ * copied, modified, or distributed except according to those terms.
+ */
+//! **The `expect!` macro**
+//!
+//! This crate adds the `expect!` macro, which is basically just calling
+//! `.expect` on your type but also:
+//!
+//! - Includes the exact line number of the error
+//! - Allows you to specify a custom error message with formatting.
+//!
+//! This gives you panic messages like this:
+//!
+//! ```no_compile
+//! thread 'example' panicked at '"expect error"', src/lib.rs:5:5
+//! ```
+//!
+//! As opposed to:
+//!
+//! ```no_compile
+//! thread 'example' panicked at 'called `Result::unwrap()` on an `Err` value: "expect error"', libcore/result.rs:945:5
+//! ```
+
+/// Unwrap a result or `panic!` with a message.
+///
+/// Works with [`Result`], [`Option`] and anything else with the `unwrap_or_else` method.
+///
+/// This macro has two forms:
+///
+/// - `expect!(result)`: calls `panic!("{:#?}", err)` on any unwrapped `Err`/`None`.
+/// - `expect!(result, ...)`: calls `panic!(...)` on any unwrapped `Err`/`None`, allowing you to
+///   specify your own error formatting. This is recommened when you are using `expect!` with
+///   [`Option`]
+///
+/// [`Result`]: https://doc.rust-lang.org/std/result/enum.Result.html
+/// [`Option`]: https://doc.rust-lang.org/std/option/enum.Option.html
+///
+/// # Example
+///
+/// Without format
+///
+/// ```rust,should_panic
+/// #[macro_use] extern crate expect_macro;
+///
+/// # fn main() {
+/// let result = Err("expect error");
+/// expect!(result);
+/// # }
+///
+/// // COMPILER OUTPUT:
+/// // thread 'example' panicked at '"expect error"', src/lib.rs:5:5
+/// ```
+///
+/// With format
+///
+/// ```rust,should_panic
+/// #[macro_use] extern crate expect_macro;
+///
+/// # fn main() {
+/// let result = Err("expect error");
+/// expect!(result, "Some values: {}, {}", 1, 2);
+/// # }
+///
+/// // COMPILER OUTPUT:
+/// // thread 'example' panicked at 'Some values: 1, 2', src/lib.rs:5:5
+/// ```
+///
+///
+#[macro_export]
+macro_rules! expect {
+    [$result:expr, $($rest:tt)*] => {
+        $result.unwrap_or_else(|_| {
+            panic!($($rest)*)
+        })
+    };
+    [$result:expr] => {
+        $result.unwrap_or_else(|e| {
+            panic!("{:?}", e)
+        })
+    };
+}
+
+#[test]
+#[should_panic]
+fn expect_panic_bare() {
+    expect!(Err("expect error"));
+}
+
+#[test]
+#[should_panic]
+fn regular_panic_bare() {
+    let result: Result<(), &str> = Err("expect error");
+    result.unwrap();
+}
+
+#[test]
+#[should_panic]
+fn expect_panic_msg() {
+    expect!(Err("expect error"), "Some values: {}, {}", 1, 2);
+}
